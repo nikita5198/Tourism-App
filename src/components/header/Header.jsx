46b0675d7785183eb5ca1073d6be8b3fe1191f54
nikita5198 <nikitap5198@gmail.com>
2023-04-14 21:59:30 +0530

    icons added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,16 @@ import logo from "../assets/images.png";
 import azadi from "../assets/azadi-moh.jpg";
 import G20 from "../assets/G20-logo.webp";
 import logo2 from '../assets/img.jpg'
-
+import { MdFlight } from 'react-icons/md';
+import { FaHotel } from 'react-icons/fa';
+import {FaBusAlt} from 'react-icons/fa'
+import { MdAirlineSeatReclineExtra } from "react-icons/md";
+import {FaHandHoldingMedical} from 'react-icons/fa'
+import {GiBookmarklet} from 'react-icons/gi'
+import {GiCruiser} from 'react-icons/gi'
+import {BsTrainFreightFrontFill} from 'react-icons/bs'
+import {GiIndianPalace} from 'react-icons/gi'
+import {GiElephantHead} from 'react-icons/gi'
 import "./header.css";
 function Header() {
   return (
@@ -38,6 +47,33 @@ function Header() {
         {/* <img className="sea" src={sea} alt="sea" /> */}
         <div className="cruise"></div>
     </div>
+
+     {/* form and components section */}
+     <div className="Home-Form-Section">
+        <div className="container home-form-container">
+          <div className="card-p">
+            <form action="">
+              <div className="form-in-custom"></div>
+              <div className="form-in-custom1"></div>
+              <div className="form-in-custom2"></div>
+            </form>
+          </div>
+        </div>
+        <div className="container-fluid nav-icon-container">
+          <ul className="nav-icons-wrap CenterIconBox">
+            <li><MdFlight /></li>
+            <li><FaHotel /></li>
+            <li><FaBusAlt /></li>
+            <li><MdAirlineSeatReclineExtra /></li>
+            <li><BsTrainFreightFrontFill /></li>
+            <li><FaHandHoldingMedical /></li>
+            <li><GiBookmarklet /></li>
+            <li><GiCruiser /></li>
+            <li><GiIndianPalace /></li>
+            <li><GiElephantHead /></li>
+          </ul>
+        </div>
+     </div>
     </>
   );
 }
